0e8a117d188b19f89d9d2d9fe73db852881eeabe
RezaBoushehri <reza.boshehry@gmail.com>
2025-03-13 15:09:07 +0330

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const path = require('path');
 
 // Use the PORT environment variable or default to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
